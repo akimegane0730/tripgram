6c971cb287f6fb655da35cf4c72a9f3605eae4d2
akimegane0730 <akimegane0730o@gmail.com>
2020-01-30 21:20:48 +0900

    create profile page

--- db/migrate/20200130023323_devise_create_users.rb
+++ db/migrate/20200130023323_devise_create_users.rb
@@ -7,6 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :nickname, null: false, unique: true
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      # t.profile_photo :string
 
       ## Recoverable
       t.string   :reset_password_token
