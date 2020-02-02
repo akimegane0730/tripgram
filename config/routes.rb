329e527f5a783fee86373b05db91cee4e7dbf814
akimegane0730 <akimegane0730o@gmail.com>
2020-02-02 14:16:51 +0900

    create likes controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :posts, only: [:new, :create, :index, :show] do
     resources :photos, only: [:create]
+
+    resources :likes, only: [:create, :destroy]
   end
 end
